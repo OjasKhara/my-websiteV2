176443a6f0c6e89b0f5d9e54a51cf5768273cedf
OjasKhara <ojas.kharabanda@axial.net>
2025-05-20 00:18:46 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DealAnalyticsDashboard from './DealAnalyticsDashboard';
+
+function App() {
+  return <DealAnalyticsDashboard />;
+}
+
+export default App;
---
+++ src/DealAnalyticsDashboard.jsx
@@ -0,0 +1,9 @@
+
+import React, { useState, useEffect, useMemo, useRef } from 'react';
+import { BarChart, Bar, PieChart, Pie, Cell, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Papa from 'papaparse';
+// ...rest of the code (not shown here due to length constraints)
+const DealAnalyticsDashboard = () => {
+  return <div>Dashboard Loaded</div>;
+};
+export default DealAnalyticsDashboard;
